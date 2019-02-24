37a7cdec477974cbc824cfaef95808663fb032f5
josefinesofie <32060043+josefinesofie@users.noreply.github.com>
2019-02-24 09:03:45 +0800

    start of navbar component

--- examensarbete/App.js
+++ examensarbete/App.js
@@ -1,13 +1,14 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
+//import { VAR1, VAR2 } from "react-native-dotenv";
+import Navbar from "../examensarbete/components/layout/Navbar";
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-
         <Text> hejhej </Text>
-
+        <Navbar />
       </View>
     );
   }
---
+++ examensarbete/components/layout/Navbar.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import { Text, View } from "react-native";
+
+class Navbar extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Hej från Navbar</Text>
+      </View>
+    );
+  }
+}
+
+export default Navbar;

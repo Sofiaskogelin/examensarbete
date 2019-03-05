18ff7f7457bda373a73275dd34f56fd74a550e8f
josefinesofie <32060043+josefinesofie@users.noreply.github.com>
2019-03-05 21:02:30 +0800

    Navbar-test

--- examensarbete/App.js
+++ examensarbete/App.js
@@ -1,19 +1,19 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
-import ChatRoom from '../examensarbete/components/chat/ChatRoom'
+import ChatRoom from "../examensarbete/components/chat/ChatRoom";
 //import { VAR1, VAR2 } from "react-native-dotenv";
 import Navbar from "../examensarbete/components/layout/Navbar";
 import SignIn from "../examensarbete/components/auth/SignIn";
 import SignUp from "../examensarbete/components/auth/SignUp";
 
-import { createStackNavigator } from 'react-navigation'
-
+import { createStackNavigator } from "react-navigation";
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
         <SignUp />
+        <Navbar />
       </View>
     );
   }
